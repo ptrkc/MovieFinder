e69bc2ec1c215ca6130c327f53d69762d2e5b258
Patrick Carneiro <40273827+ptrkc@users.noreply.github.com>
2021-04-13 19:21:50 -0300

    you can now buy movies, with intentional random failures to do so

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,41 @@
-const movieListURL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes"
-const movieListRequest = axios.get(movieListURL)
+const movieListURL = "https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes";
+const movieListRequest = axios.get(movieListURL);
 movieListRequest.then(treatData);
-let movieArray = []
-let moviesHTML = ""
+let movieArray = [];
+let moviesHTML = "";
 
 function treatData(response) {
-    movieArray = response.data
+    movieArray = response.data;
     console.log(movieArray)
     for (let i = 0; i < movieArray.length; i++) {
         moviesHTML += `
     <div class="movie">
     <img src="${movieArray[i].imagem}">
     <div class="title">${movieArray[i].titulo}</div>
-    <button onclick="buyMovie(${movieArray[i].id})">
+    <button onclick="buyMovie('${movieArray[i].id}')">
       Comprar
       <ion-icon name="cart-outline"></ion-icon>
     </button>
     </div>
-    `
+    `;
     };
     document.querySelector(".movies").innerHTML = moviesHTML
+}
+
+function buyMovie(id) {
+    const name = prompt("Qual seu nome?");
+    const quantity = parseInt(prompt("Quantos ingressos?"));
+    const data = {
+        nome: name,
+        quantidade: quantity
+    };
+    const buyRequest = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/moviefinder/filmes/${id}/ingresso`, data);
+    buyRequest.then(buyOK);
+    buyRequest.catch(buyError);
+}
+function buyOK(response) {
+    alert("Ingresso comprado com sucesso!")
+}
+function buyError(response) {
+    alert("Os ingressos para este filme estão esgotados!")
 }
